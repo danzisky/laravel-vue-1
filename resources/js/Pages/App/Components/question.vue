78fb6e211eaaba71d6ae9809c6fde3f2274275c0
Nwaeke Daniel <dannie005@gmail.com>
2022-09-04 19:09:02 +0300

    added some transitions on the questions component

--- resources/js/Pages/App/Components/question.vue
+++ resources/js/Pages/App/Components/question.vue
@@ -1,22 +1,26 @@
 <template>
-    <div v-if="question.isCurrent" class="p-4 bg-gray-200 rounded-lg">
-        <div class="">
-            <div class="flex p-4 bg-blue-400 rounded-lg text-white text-lg">
-                <div class="px-3 w-max font-bold mr-2">
-                    Q
+    <Transition name="bounce">
+        <div v-if="question.isCurrent" class="p-4 bg-gray-200 rounded-lg" >
+            <!-- :class="[question.isCurrent ? 'transition ease-linear ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300' : '', ]" -->
+            <div class="">
+                <div class="flex p-4 bg-blue-400 rounded-lg text-white text-lg">
+                    <div class="px-3 w-max font-bold mr-2">
+                        Q
+                    </div>
+                    <div>
+                        {{ question.question }}
+                    </div>
                 </div>
-                <div>
-                    {{ question.question }}
-                </div>
-            </div>
-            <div class="m-auto p-8 flex flex-col space-y-4">
-                <div class="space-y-4">
-                    <Answer v-for="(answer, index) in answers" :key="answer.id" :answer="answer" :selectedOption="question.selectedOption" :index="index"
-                        @click="$emit('selectOption', questionIndex, index)" />
+                <div class="m-auto p-8 flex flex-col space-y-4">
+                    <div class="space-y-4">
+                        <Answer v-for="(answer, index) in answers" :key="answer.id" :answer="answer"
+                            :selectedOption="question.selectedOption" :index="index"
+                            @click="$emit('selectOption', questionIndex, index)" />
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </Transition>
 </template>
 
 <script>
@@ -44,3 +48,27 @@ export default {
     }
 }
 </script>
+
+<style>
+.bounce-enter-active {
+    animation: bounce-in 0.7s;
+}
+
+.bounce-leave-active {
+    animation: bounce-in 0s reverse;
+}
+
+@keyframes bounce-in {
+    0% {
+        transform: scale(0.4);
+    }
+
+    50% {
+        transform: scale(1.1);
+    }
+
+    100% {
+        transform: scale(1);
+    }
+}
+</style>
