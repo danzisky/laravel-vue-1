0fa6523b971250ae8ce6555eca28713a5b0a27fb
Nwaeke Daniel <dannie005@gmail.com>
2022-09-07 18:15:07 +0300

    finally fixed the personality formula

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -20,8 +20,10 @@ class QuestionController extends Controller
         return QuestionResource::collection(Question::all());
     }
 
-    /* public function deductPersonality(Request $request) {
+    public function deductPersonality(Request $request) {
         // dd($request->testResponse);
+        define('maxAnswerRank', 4);
+        define('minAnswerRank', 1);
         $testResponse = collect($request->testResponse) ?? [];
 
         $extrovertScore = 0;
@@ -31,29 +33,50 @@ class QuestionController extends Controller
         // $responseRanks = array_column($testResponse, 'rank');
         $totalQuestions = 0;
         $totalScore = 0;
+        $totalScoreInt = 0;
         foreach ($testResponse as $response) {
             // given the answers are for evenly increasing levels of personality difference from one extreme to the other
 
             // using extrovertedness as reference
-            $totalScore = $response['peak_personality'] == "extrovert" ? $response['answer_rank'] : 4 - $response['answer_rank'];
+            // $response['answer_rank']--;
+            $responseRank = $response['answer_rank'] - 1;
+            $totalScore += ($response['peak_personality'] == "extrovert" ? $responseRank : 3 - $responseRank)/3 *4;
+            $totalScoreInt += ($response['peak_personality'] == "extrovert" ? 3 - $responseRank : $responseRank)/3 * 4;
+
+            /* $totalScore = $response['peak_personality'] == "extrovert" ? $response['answer_rank'] : (maxAnswerRank + minAnswerRank) - $response['answer_rank']; */
+            /* $totalScoreInt = 
+            (maxAnswerRank + minAnswerRank) - $response['peak_personality'] == "extrovert" ? $response['answer_rank'] : 4 - $response['answer_rank']; */
 
             $totalQuestions++;
         }
 
         $extrovertedness = $totalScore/($totalQuestions * 4);
+        $introvertedness = $totalScoreInt/($totalQuestions * 4);
+        // $introvertedness = (1 - $extrovertedness);
         // pure extrovert has an 'extrvertedness' score of 1
 
-        $extrovertScore = $extrovertedness * 4;
-        $introvertScore = (1 - $extrovertedness) * 4;
-        
+        $netScore = $extrovertedness - $introvertedness;
+
+        // $extrovertScore = $netScore > 0 ? $netScore : 1 - abs($netScore); //since extrovertedness is reference point
+        // $introvertScore = $netScore < 0 ? abs($netScore) : 1 - abs($netScore);
+
+        // $introvertScore = (1 - $extrovertedness) * 4;
+        $introvertScore = $introvertedness;
+        $extrovertScore = $extrovertedness;
         $personalityResponse = [
+            'netScore' => $netScore,
+            'totalQuestions' => $totalQuestions,
+            'totalScoreInt' => $totalScoreInt,
+            'totalScore' => $totalScore,
+            'extrovertedness' => $extrovertedness,
+            'introvertedness' => $introvertedness,
             'introvertScore' => $introvertScore,
             'extrovertScore' => $extrovertScore,
         ];
         $personalityResponse = collect($personalityResponse);
         return $personalityResponse->toJson();
-    } */
-    public function deductPersonality(Request $request) {
+    }
+    /* public function deductPersonality(Request $request) {
         // dd($request->testResponse);
         $testResponse = collect($request->testResponse) ?? [];
 
@@ -111,7 +134,7 @@ class QuestionController extends Controller
         ];
         $personalityResponse = collect($personalityResponse);
         return $personalityResponse->toJson();
-    }
+    } */
     /**
      * Show the form for creating a new resource.
      *
