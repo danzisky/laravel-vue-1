e4b362b8a04864bf21aadbe223e48a538949d75c
Nwaeke Daniel <dannie005@gmail.com>
2022-09-14 01:40:19 +0300

    added more test cases

--- tests/Feature/ResourceTest.php
+++ tests/Feature/ResourceTest.php
@@ -9,6 +9,9 @@ use Tests\TestCase;
 
 class ResourceTest extends TestCase
 {
+    use RefreshDatabase;
+    
+    
     /**
      * A basic feature test example.
      *
@@ -16,6 +19,7 @@ class ResourceTest extends TestCase
      */
     public function test_can_fetch_questions_over_api()
     {
+        $this->seed();
         $response = $this->getJson('/api/questions');
 
         $response->assertJson(fn (AssertableJson $json) =>
@@ -23,4 +27,42 @@ class ResourceTest extends TestCase
                 ->missing('message')
         );
     }
+    
+    public function test_api_response_contains_question()
+    {
+        $this->seed();
+        $response = $this->getJson('/api/questions');
+
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->has('data', 4)
+                ->has('data.0', fn ($json) =>
+                $json->has('question')
+                    ->has('peak_personality')
+                    ->has('anchor_rank')
+                    ->whereType('anchor_rank',['double', 'integer', 'float'])
+                    ->etc()
+            )
+        );
+    }
+
+    public function test_api_response_questions_contain_answers()
+    {
+        $this->seed();
+        $response = $this->getJson('/api/questions');
+
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->has('data', 4)
+                ->has('data.0', fn ($json) =>
+                    $json->has('answers', 4)
+                        ->has('answers.0', fn ($json) =>
+                            $json->has('answer')
+                                ->has('rank')
+                                ->whereType('rank', 'integer')
+                                ->etc()
+                        )
+                ->etc()
+            )
+        );
+    }
+    
 }
